9d15041f8272878c2ac97d9e1012a3144b40827b
shunku <s061064@gmail.com>
2020-09-01 00:49:20 +0900

    Added material-ui & Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./styles.css";
+import { Container } from "@material-ui/core";
+
+import Navbar from "./components/navbar/Navbar.component";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox + Netlify</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar />
+      <Container>
+        <h1>Hello CodeSandbox + Netlify</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </Container>
     </div>
   );
 }
